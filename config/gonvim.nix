8f9999a5d71eb3c9e52f2ae765ff28e2c4600639
kennardpeters <kenpo1999@gmail.com>
2025-09-06 15:58:08 -0700

    added keymaps for running go tests

--- config/gonvim.nix
+++ config/gonvim.nix
@@ -15,4 +15,42 @@
         group = format_sync_grp,
       })
   '';
+  keymaps = [
+    # Go run test
+    # vim.api.nvim_set_keymap('n', '<leader>rt', ":GoTestFunc<CR>", { noremap = true })
+    {
+      mode = "n";
+      key = "<leader>rt";
+      action = ":GoTestFunc<CR>";
+      options = {
+        silent = true;
+        desc = "Run Go Test";
+        noremap = true;
+      };
+    }
+    # -- run test w/ detail
+    # vim.api.nvim_set_keymap('n', '<leader>rd', ":GoTestFunc -v<CR>", { noremap = true })
+    {
+      mode = "n";
+      key = "<leader>rd";
+      action = ":GoTestFunc -v<CR>";
+      options = {
+        silent = true;
+        desc = "Run Go Test w/ Detail";
+        noremap = true;
+      };
+    }
+    # -- clean test cache
+    # vim.api.nvim_set_keymap('n', '<leader>ct', ":!go clean -testcache<CR>", { noremap = true })
+    {
+      mode = "n";
+      key = "<leader>ct";
+      action = ":!go clean -testcache<CR>";
+      options = {
+        silent = true;
+        desc = "Clean Go test cache";
+        noremap = true;
+      };
+    }
+  ];
 }
